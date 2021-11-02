9d2db8ce93cbd1394b6862c9bbfc7b00e4b44dc3
Jacob Kravits <kravitsjacob@gmail.com>
2021-11-02 13:00:11 -0600

    Test for get_generator_information

--- src/analysis_testing.py
+++ src/analysis_testing.py
@@ -4,6 +4,7 @@ import unittest
 
 import pandas as pd
 import pandas.testing as pd_testing
+import pandapower.networks
 
 import analysis
 
@@ -17,6 +18,20 @@ class AnalysisLib(unittest.TestCase):
 
     def setUp(self):
         self.addTypeEqualityFunc(pd.DataFrame, self.assertDataframeEqual)
+
+    def test_get_generator_information(self):
+        # Setup
+        net = pandapower.networks.case_ieee30()
+
+        # Run
+        df_gen_info = analysis.get_generator_information(net)
+
+        # Check
+        test_bus = net.gen.iloc[0]['bus']
+        test_power_max = net.gen.iloc[0]['max_p_mw']
+        expected_power_max = df_gen_info[df_gen_info['bus'] == test_bus]['max_p_mw'][0]
+
+        self.assertEqual(test_power_max, expected_power_max)
 
     def test_grid_sample(self):
         # Setup
