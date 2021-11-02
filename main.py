d1010aa0603781e5a5735738de8d505a3854ee8b
Jacob Kravits <kravitsjacob@gmail.com>
2021-11-02 13:14:13 -0600

    Sample decision space

--- main.py
+++ main.py
@@ -8,12 +8,26 @@ from src import analysis
 
 def main():
     # Inputs
+    n_steps = 2
     inputs = analysis.input_parse()
     df_coef = pd.read_csv(inputs['path_to_df_coef'])
     net = pandapower.networks.case_ieee30()
 
-    # Get coefficients
+    # Get coefficients TODO
     a = 1
+
+    # Sample decision space
+    df_gen_info = analysis.get_generator_information(net)
+    df_gen_info = df_gen_info[df_gen_info['et'] != 'ext_grid']  # External grid will be solved during power flow
+    df_gridspecs = pd.DataFrame(
+        {
+            'var': ('bus ' + df_gen_info['bus'].astype(str)).tolist(),
+            'min': df_gen_info['min_p_mw'].tolist(),
+            'max': df_gen_info['max_p_mw'].tolist(),
+            'steps': n_steps
+        }
+    )
+    df_grid = analysis.grid_sample(df_gridspecs)
 
     # solve opf
 
