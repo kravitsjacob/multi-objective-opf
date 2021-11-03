a551e1bb5ec11bad6a9ee2cc072154db6aecbc93
Jacob Kravits <kravitsjacob@gmail.com>
2021-11-03 10:13:50 -0600

    Add path_to_config input in input_parse

--- src/analysis.py
+++ src/analysis.py
@@ -4,15 +4,24 @@ import os
 import configparser
 import argparse
 import itertools
+import copy
 
 import numpy as np
 import pandas as pd
+import pandapower as pp
 
 
-def input_parse():
+def input_parse(path_to_config=None):
     """
-    Parse inputs
-    :return: dict
+
+    Parameters
+    ----------
+    path_to_config: str
+        Path to config file (gets preference over command line)
+
+    Returns
+    -------
+    inputs: dict
         Dictionary of inputs
     """
     # Local vars
@@ -34,6 +43,8 @@ def input_parse():
 
     # Parse config file
     config_inputs.read(argparse_inputs.config_file)
+    if path_to_config:
+        config_inputs.read(path_to_config)
 
     # Store inputs
     path_to_data = config_inputs['MAIN IO']['data']
@@ -45,7 +56,7 @@ def input_parse():
     return inputs
 
 
-def get_generator_information(net):
+def get_generator_information(net, gen_types=('gen', 'sgen', 'ext_grid')):
     """
     Get DataFrame summarizing all the generator information
 
@@ -53,6 +64,8 @@ def get_generator_information(net):
     ----------
     net: pandapowerNet
         Network to summarize
+    gen_types: list
+        Generator types
 
     Returns
     -------
@@ -60,13 +73,13 @@ def get_generator_information(net):
         Summary of generator information in `net`
     """
     # Initialize local vars
-    gen_types = ['gen', 'sgen', 'ext_grid']
     df_gen_info = pd.DataFrame()
 
     # Convert generator information dataframe
     for gen_type in gen_types:
         df_temp_gen_info = getattr(net, gen_type)
         df_temp_gen_info['et'] = gen_type
+        df_temp_gen_info['element'] = df_temp_gen_info.index.astype(float)
         df_gen_info = df_gen_info.append(df_temp_gen_info)
     df_gen_info = df_gen_info.reset_index(drop=True)  # Important to eliminate duplicated indices
 
@@ -91,9 +104,40 @@ def grid_sample(df_gridspecs):
     # Get linear spaces
     linspace_list = []
     for i, row in df_gridspecs.iterrows():
-        linspace_list.append(np.linspace(row['min'], row['max'], row['steps']))
+        linspace_list.append(np.linspace(row['min'], row['max'], int(row['steps'])))
 
     # Create dataframe
     df_grid = pd.DataFrame(list(itertools.product(*linspace_list)), columns=df_gridspecs['var'].tolist())
 
     return df_grid
+
+
+def mo_opf(ser_decisions, net):
+    # Local vars
+    t = 5 * 1 / 60 * 1000  # minutes * hr/minutes * kw/MW
+    net = copy.deepcopy(net)
+
+    # Apply decision to network
+    net.gen = net.gen.merge(ser_decisions, left_on='bus', right_index=True)
+    net.gen['p_mw'] = net.gen[0]
+
+    # Solve powerflow
+    pp.rundcpp(net)
+
+    # Formatting results
+    df_obj = get_generator_information(net, ['res_gen', 'res_ext_grid'])
+    df_obj = df_obj.merge(net.df_coef, left_on=['element', 'et'], right_on=['element', 'et'])
+
+    # Compute objectives terms
+    df_obj['F_cost'] = df_obj['a'] + df_obj['b'] * (df_obj['p_mw']/100) + df_obj['c'] * (df_obj['p_mw']/100)**2
+    df_obj['F_emit'] = \
+        0.01 * df_obj['alpha'] +\
+        0.01 * df_obj['beta_emit'] * (df_obj['p_mw']/100) +\
+        0.01 * df_obj['gamma'] * (df_obj['p_mw']/100)**2 +\
+        df_obj['xi'] * np.exp(df_obj['lambda'] * (df_obj['p_mw']/100))
+    df_obj['F_with'] = df_obj['beta_with'] * df_obj['p_mw'] * t
+    df_obj['F_con'] = df_obj['beta_with'] * df_obj['p_mw'] * t
+
+    # Compute objectives
+
+    return ser_objectives
