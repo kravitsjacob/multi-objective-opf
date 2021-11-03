b4a79832aa9915b252bae9a771ee06446ce607cf
Jacob Kravits <kravitsjacob@gmail.com>
2021-11-03 16:43:36 -0600

    Added pymoo dependencies via pip to run.sh

--- run.sh
+++ run.sh
@@ -9,5 +9,8 @@ conda env create -f multi-objective-opf-env.yml
 # Load conda environment
 conda activate multi-objective-opf-env
 
+# Install non-conda packages
+pip install -U pymoo
+
 # Run analysis
 python main.py -c config.ini
