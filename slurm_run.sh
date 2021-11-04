20d284ff78313f5e4ceaf8a43b82ad9f0c918845
kravitsjacob <kravitsjacob@gmail.com>
2021-11-04 11:59:04 -0600

    Updated slurm_run.sh for 32768 iteration

--- slurm_run.sh
+++ slurm_run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=4
-#SBATCH --cpus-per-task=2
-#SBATCH --mem-per-cpu=1000
+#SBATCH --ntasks-per-node=16
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=100
 #SBATCH --output=run.out
 #SBATCH --job-name=gosox
 #SBATCH --partition=shas
 #SBATCH --qos=condo
 #SBATCH --account=ucb-summit-jrk
-#SBATCH --time=0-00:05:00
+#SBATCH --time=0-00:06:00
 #SBATCH --mail-user=kravitsjacob@gmail.com
 #SBATCH --mail-type=END
 
@@ -25,5 +25,5 @@ conda activate multi-objective-opf-env
 # Install non-conda packages
 pip install -U pymoo
 
-# Run analysis
-python -u main.py -c config.ini --n_tasks 8 --n_steps 6
+# Run analysis (only grid search, not nondom)
+python -u main.py -c config.ini --n_tasks 32 --n_steps 8
