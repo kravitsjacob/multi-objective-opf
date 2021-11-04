1ce1cdfd700fb00bb70452dcd93cf3ea52688ba7
Jacob Kravits <kravitsjacob@gmail.com>
2021-11-04 13:46:47 -0600

    Add pareto install to run.sh

--- run.sh
+++ run.sh
@@ -11,6 +11,8 @@ conda activate multi-objective-opf-env
 
 # Install non-conda packages
 pip install -U pymoo
+git clone https://github.com/matthewjwoodruff/pareto.py.git pareto
+git clone https://github.com/matthewjwoodruff/pareto.py.git src/pareto
 
 # Run analysis
 python main.py -c config.ini
