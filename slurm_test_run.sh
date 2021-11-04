02ffd0db98448cccfdac40ec1e417a16f3713ba3
kravitsjacob <kravitsjacob@gmail.com>
2021-11-04 15:01:35 -0600

    Change slurm_test_run resources to work for conda env creation

--- slurm_test_run.sh
+++ slurm_test_run.sh
@@ -3,16 +3,17 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=2
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=1000
+#SBATCH --mem-per-cpu=5000
 #SBATCH --output=run.out
 #SBATCH --job-name=gosox
 #SBATCH --partition=shas
 #SBATCH --qos=condo
 #SBATCH --account=ucb-summit-jrk
-#SBATCH --time=0-00:01:00
+#SBATCH --time=0-00:06:00
 #SBATCH --mail-user=kravitsjacob@gmail.com
 #SBATCH --mail-type=END
 
+# Setup
 module purge
 source /curc/sw/anaconda3/2019.07/bin/activate
 
@@ -28,4 +29,4 @@ git clone https://github.com/matthewjwoodruff/pareto.py.git pareto
 git clone https://github.com/matthewjwoodruff/pareto.py.git src/pareto
 
 # Run analysis
-python -u main.py -c config.ini --n_tasks 2 --n_steps 2
+python -u main.py -c config.ini --n_tasks 3 --n_steps 2
